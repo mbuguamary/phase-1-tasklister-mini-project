a9012634aba0045bdbe0aac1ed731c131b4f7ef0
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-08-25 14:50:25 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
+  const form=document.getElementById("create-task-form");
+  const tasksContainer=document.getElementById('tasks');
+  form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+
+    const description=document.getElementById('new-task-description').value
+     const itemElement =document.createElement('li');
+     itemElement.innerHTML=description;
+     tasksContainer.appendChild(itemElement);
+  
+  })
 });
